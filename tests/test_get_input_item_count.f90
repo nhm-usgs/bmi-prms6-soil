cd2677462215b906114b3b4be8c3f9a14e1a368e
Mark Piper <mark.piper@colorado.edu>
2020-03-18 15:43:26 -0600

    implement info and exchange item functions

---
+++ tests/test_get_input_item_count.f90
@@ -0,0 +1,18 @@
+program test_get_input_item_count
+
+  use bmif_2_0, only: BMI_FAILURE
+  use bmiprmssoil
+  use fixtures, only: status
+
+  implicit none
+
+  integer, parameter :: expected = 33
+  type (bmi_prms_soil) :: m
+  integer :: count
+
+  status = m%get_input_item_count(count)
+  
+  if (count /= expected) then
+     stop BMI_FAILURE
+  end if
+end program test_get_input_item_count
