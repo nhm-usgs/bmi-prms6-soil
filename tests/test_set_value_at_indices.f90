bcc2353a34bce54ac3f24cc99efe647d6c6b6a0f
Richard R. McDonald <rmcd@usgs.gov>
2020-03-21 09:52:29 -0600

    finished test_set_value_at_indices.  Again this is sparse but no other var types of equivalent get function of testing.

--- tests/test_set_value_at_indices.f90
+++ tests/test_set_value_at_indices.f90
@@ -15,136 +15,49 @@
         stop BMI_FAILURE
     end if
 
-    retcode = test2()
-    if (retcode.ne.BMI_SUCCESS) then
-        stop BMI_FAILURE
-    end if
-
     contains
 
   ! Test getting r32 hru_area.
-  function test1() result(code)
+function test1() result(code)
     character (len=*), parameter :: &
-         var_name = "hru_ppt"
-    integer, parameter :: rank = 1
+         var_name = "pref_flow_den"
+    integer, parameter :: rank =1
     integer, parameter :: size = 7
     integer, parameter :: fsize = 14
-
     integer, parameter, dimension(rank) :: shape = (/ 7 /)
     integer, parameter, dimension(rank) :: fshape = (/ 14 /)
     integer, parameter, dimension(size) :: &
        indices = (/ 2, 4, 6, 8, 10, 12, 14 /)
-    real, parameter, dimension(size) :: &
-         setv = (/0.25,0.25,0.25,0.25,0.25,0.25,0.25/)
-    real, parameter, dimension(fsize) :: &
-         full_expected = (/0.0, 0.25, 0.0, 0.25, 0.0, 0.25, 0.0, 0.25, 0.0, 0.25, 0.0, 0.25, 0.0, 0.25/)
-    real :: tval(size), ftval(fsize)
+
+    real, parameter :: expected(fsize) = (/ 0.0, 0.25, 0.0, 0.25, 0.0, 0.25, 0.0, &
+                                              0.25, 0.0, 0.25, 0.0, 0.25, 0.0, 0.25 /)
+    real :: val(size), fval(fsize)
+    real :: setv(size), fsetv(fsize)
     integer :: i, code
-    
-    status = m%initialize(config_file)
-    status = m%update()
-    status = m%get_value_at_indices(var_name, tval, indices)
-    status = m%set_value_at_indices(var_name, indices, setv)
-    status = m%get_value_at_indices(var_name, tval, indices)
-    status = m%get_value(var_name, ftval)
-    status = m%finalize()
+
+    code = m%initialize(config_file)
+    code = m%get_value(var_name, fval)
+    code = m%get_value_at_indices(var_name, val, indices)
+    val = 0.25
+    code = m%set_value_at_indices(var_name, indices, val)
+    code = m%get_value_at_indices(var_name, val, indices)
+    code = m%get_value(var_name, fval)
+    code = m%finalize()
     
     ! Visual inspection.
     write(*,*) "Test 1"
-    write(*,*) "epected"
-    call print_1darray(setv, shape)
-    write(*,*) "set value"
-    call print_1darray(tval, shape)
+    write(*,*) "Expected"
+    call print_1darray(expected, fshape)
+    write(*,*) "Set Value"
+    call print_1darray(fval, fshape)
 
     code = BMI_SUCCESS
-    do i = 1, shape(1)
-       if (isreal4equalreal4(setv(i), tval(i)).neqv..TRUE.) then
+    do i = 1, fsize
+       if (fval(i).ne.expected(i)) then
           code = BMI_FAILURE
-          exit
        end if
     end do
-    
-    ! Visual inspection.
-    write(*,*) "Test 1 full value"
-    write(*,*) "epected"
-    call print_1darray(full_expected, fshape)
-    write(*,*) "set value"
-    call print_1darray(ftval, fshape)
-
-    code = BMI_SUCCESS
-    do i = 1, fshape(1)
-       if (isreal4equalreal4(full_expected(i), ftval(i)).neqv..TRUE.) then
-          code = BMI_FAILURE
-          exit
-       end if
-    end do
-
   end function test1
 
-  function test2() result(code)
-    character (len=*), parameter :: &
-         var_name = "dprst_vol_open"
-    
-    integer, parameter :: rank = 1
-    integer, parameter :: size = 7
-    integer, parameter :: fsize = 14
-    integer, parameter, dimension(rank) :: shape = (/ 7 /)
-    integer, parameter, dimension(rank) :: fshape = (/ 14 /)
-    integer, parameter, dimension(size) :: &
-       indices = (/ 2, 4, 6, 8, 10, 12, 14 /)
-    double precision, parameter :: expected(size) = (/ &
-        224.910984328835, 8057.99076883995, 10864.8073074743, &
-        253.518300425138, 1121.18710758845, 0.000000000000000, &
-        12585.9414342218 /)
-
-    double precision, parameter :: fexpected(fsize) = (/ &
-        41558.0385172600, 224.910984328835, 5545.71725420578, 8057.99076883995, &
-        3514.98316578602, 10864.8073074743, 3220.60291601327, 253.518300425138, &
-        556.593107720004, 1121.18710758845, 88.9853008034370, 0.00000000000000, &
-        259.981427881226, 12585.9414342218 /)
-    double precision :: setv(size), fsetv(fsize)
-    double precision :: val(size), fval(fsize)
-    integer :: i, code
-    
-    status = m%initialize(config_file)
-    status = m%get_value(var_name, fval)
-    status = m%get_value_at_indices(var_name, val, indices)
-    setv = 1.5*val
-    status = m%set_value_at_indices(var_name, indices, setv)
-    status = m%get_value_at_indices(var_name, val, indices)
-    status = m%get_value(var_name, fval)
-    status = m%finalize()
-
-    ! Visual inspection.
-    write(*,*) "Test 2"
-    write(*,*) "Expected"
-    write(*,*) setv
-    write(*,*) "set value"
-    write(*,*) val
-
-    code = BMI_SUCCESS
-    do i = 1, size
-       if (isreal8equalreal8(setv(i), val(i)).neqv..TRUE.) then
-          code = BMI_FAILURE
-       end if
-    end do
-    
-        ! Visual inspection.
-    write(*,*) "Test 2 full value"
-    write(*,*) "Expected"
-    write(*,*) fexpected
-    write(*,*) "set value"
-    write(*,*) fval
-    
-    !the above is correct but test below doesn't work, there are some precision issues I 
-    !don't understand...
-    
-    !code = BMI_SUCCESS
-    !do i = 1, size
-    !   if (isreal8equalreal8(fexpected(i), fval(i)).ne..TRUE.) then
-    !      code = BMI_FAILURE
-    !   end if
-    !end do
-  end function test2
 
     end program test_set_value_at_indices
