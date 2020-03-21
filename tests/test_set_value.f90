2bae40ee6dc2d71eed11651163c951f9e6d54d60
Richard R. McDonald <rmcd@usgs.gov>
2020-03-21 09:41:42 -0600

    finished test_set_value.  Its sparse but the only var type that has set/get is r32.  Also the irf_ex do a lot of set/get so I feel this is adequate.  BUT if i'm off base please let me know and I'll think about what else to include in this test

--- tests/test_set_value.f90
+++ tests/test_set_value.f90
@@ -15,84 +15,38 @@
         stop BMI_FAILURE
     end if
 
-    retcode = test2()
-    if (retcode.ne.BMI_SUCCESS) then
-        stop BMI_FAILURE
-    end if
-
     contains
 
   ! Test getting r32 hru_area.
   function test1() result(code)
     character (len=*), parameter :: &
-         var_name = "hru_ppt"
-    integer, parameter :: rank = 1
+         var_name = "pref_flow_den"
     integer, parameter :: size = 14
-    integer, parameter, dimension(rank) :: shape = (/ 14 /)
-    real, parameter, dimension(shape(1)) :: &
-         setv = (/0.25,0.25,0.25,0.25,0.25, &
-        0.25,0.25,0.25,0.25,0.25,0.25, &
-        0.25,0.25,0.25 /)
-
-    real :: tval(size)
+    integer :: dims(1) = size
+    real, parameter :: expected(size) = (/ 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, &
+                                              0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25 /)
+    real :: val(size), val2(size)
     integer :: i, code
 
-    status = m%initialize(config_file)
-    status = m%update()
-    status = m%get_value(var_name, tval)
-    status = m%set_value(var_name, setv)
-    status = m%get_value(var_name, tval)
-    status = m%finalize()
-
+    code = m%initialize(config_file)
+    code = m%get_value(var_name, val)
+    val = 0.25
+    code = m%set_value(var_name, val)
+    code = m%get_value(var_name, val2)
+    code = m%finalize()
+    
     ! Visual inspection.
     write(*,*) "Test 1"
-    call print_1darray(tval, shape)
-    do i = 1, shape(1)
-       write(*,*) tval(i)
-    end do
+    call print_1darray(val, dims)
+    call print_1darray(val2, dims)
 
     code = BMI_SUCCESS
-    do i = 1, shape(1)
-       if (isreal4equalreal4(setv(i), tval(i)).neqv..TRUE.) then
+    do i = 1, size
+       if (val2(i).ne.expected(i)) then
           code = BMI_FAILURE
-          exit
        end if
     end do
   end function test1
 
-  !test r64 by nhru
-  function test2() result(code)
-    character (len=*), parameter :: &
-         var_name = "dprst_vol_open"
-    integer, parameter :: size = 14
-    double precision, parameter :: expected(size) = (/ &
-        41558.0385172600, 149.940656219223, 5545.71725420578, 5371.99384589330, &
-        3514.98316578602, 7243.20487164950, 3220.60291601327, 169.012200283425, &
-        556.593107720004, 747.458071725632, 88.9853008034370, 0.00000000000000, &
-        259.981427881226, 8390.62762281456 /)
-    double precision :: setv(size) 
-    double precision :: val(size)
-    integer :: i, code
-    
-    setv = 1.5*expected
-
-    status = m%initialize(config_file)
-    status = m%get_value(var_name, val)
-    status = m%set_value(var_name, setv)
-    status = m%get_value(var_name, val)
-    status = m%finalize()
-
-    ! Visual inspection.
-    write(*,*) "Test 5"
-    write(*,*) val
-    write(*,*) setv
-
-    code = BMI_SUCCESS
-    do i = 1, size
-       if (isreal8equalreal8(setv(i), val(i)).neqv..TRUE.) then
-          code = BMI_FAILURE
-       end if
-    end do
-  end function test2
   
 end program test_set_value
