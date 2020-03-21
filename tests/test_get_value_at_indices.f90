27c1608154c1f7996733543e8dcef2058072cffe
Richard R. McDonald <rmcd@usgs.gov>
2020-03-21 08:11:17 -0600

    finish test_get_value_at_indices, test for r32 by nhru and r64 by nhru

--- tests/test_get_value_at_indices.f90
+++ tests/test_get_value_at_indices.f90
@@ -3,7 +3,7 @@
     use bmif_2_0, only: BMI_SUCCESS, BMI_FAILURE
     use bmiprmssoil
     use fixtures, only: config_file, status, print_1darray, print_i_1darray, &
-        isreal4equalreal4
+        isreal4equalreal4, print_d_1darray
 
     implicit none
 
@@ -12,158 +12,93 @@
 
 
     retcode = test1()
-    !if (retcode.ne.BMI_SUCCESS) then
-    !    stop BMI_FAILURE
-    !end if
+    if (retcode.ne.BMI_SUCCESS) then
+        stop BMI_FAILURE
+    end if
 
     retcode = test2()
     if (retcode.ne.BMI_SUCCESS) then
         stop BMI_FAILURE
     end if
     
-    retcode = test6()
-    if (retcode.ne.BMI_SUCCESS) then
-        stop BMI_FAILURE
-    end if
+    !retcode = test6()
+    !if (retcode.ne.BMI_SUCCESS) then
+    !    stop BMI_FAILURE
+    !end if
+    
     contains
 
-! Test getting i32 hru_type.
-    function test1() result(code)
+! Test getting r32 hru_type.
+function test1() result(status)
     character (len=*), parameter :: &
-        var_name = "hru_type"
+         var_name = "ssr2gw_exp"
     integer, parameter :: rank = 1
     integer, parameter :: size = 7
     integer, parameter, dimension(rank) :: shape = (/ 7 /)
     integer, parameter, dimension(7) :: &
        indices = (/ 2, 4, 6, 8, 10, 12, 14 /)
-    integer, parameter, dimension(7) :: &
-        expected = (/1,1,1,1,1,1,1 /)
-    integer, allocatable :: tval(:)
-    integer :: i, code
-
-    allocate(tval(size))
-    code = m%initialize(config_file)
-    code = m%get_value_at_indices(var_name, tval, indices)
-    code = m%finalize()
-
-    ! Visual inspection.
-    write(*,*) "Test 1"
-    call print_i_1darray(tval, shape)
-    do i = 1, size
-        write(*,*) tval(i)
-    end do
-
-    code = BMI_SUCCESS
-    do i = 1, size
-        if (tval(i).ne.expected(i)) then
-            code = BMI_FAILURE
-            exit
-        end if
-    end do
-
-    end function test1
-
-! Test getting r32 hru_area.
-    function test2() result(code)
-    character (len=*), parameter :: &
-        var_name = "infil"
-    integer, parameter :: rank = 1
-    integer, parameter :: size = 7
-    integer, parameter, dimension(rank) :: shape = (/ 7 /)
-    integer, parameter, dimension(7) :: &
-       indices = (/ 2, 4, 6, 8, 10, 12, 14 /)
-    real, parameter, dimension(7) :: &
-        expected = (/0.2061947,0.2881884, &
-        0.2354067,0.2170401,0.4194541, &
-        0.2514547,0.1666235/)
-    real, allocatable :: tval(:)
-    integer :: i, code
+    real, parameter, dimension(size) :: &
+         expected = (/ 1.20, 1.20, 1.20, 1.20, &
+                      1.20, 1.20, 1.20 /)
+    real :: tval(size)
+    integer :: i, status
+    double precision :: endtime
     
-    allocate(tval(size))
-    
-    code = m%initialize(config_file)
-    do i =1,79
-        status = m%update()
-    enddo
-    !status = m%update_until(68.0d0)
-    code = m%get_value_at_indices(var_name, tval, indices)
-    code = m%finalize()
-
-
-    ! Visual inspection.
-    write(*,*) "Test 2"
-    call print_1darray(tval, shape)
-    do i = 1, size
-        write(*,*) expected(i)
-    end do
-
-    code = BMI_SUCCESS
-    do i = 1, size
-        if (isreal4equalreal4(expected(i), tval(i)).neqv..TRUE.) then
-            code = BMI_FAILURE
-            exit
-        end if
-    end do
-    end function test2
-  function test6() result(code)
-    character (len=*), parameter :: var_name = "dday_intcp"
-    integer, parameter :: rank = 2
-    !integer, parameter :: size = ‭168‬
-    integer, parameter, dimension(rank) :: shape = (/ 14, 12 /)
-    integer, parameter, dimension(shape(2)) :: &
-       indices = (/ 1, 15, 29, 43, 57, 71, 85, 99, 113, 127, 141, 155 /)
-    real, parameter, dimension(shape(2)) :: &
-         expected = (/ -10.0, -11.0, -13.0, -16.0, -20.0, -25.0, -30.0, &
-                       -25.0, -20.0, -16.0, -13.0, -11.0 /)
-    real :: tval(shape(2))
-    integer :: i, code
-
     status = m%initialize(config_file)
-    status = m%update()
     status = m%get_value_at_indices(var_name, tval, indices)
     status = m%finalize()
 
     ! Visual inspection.
-    write(*,*) "Test 6 get values"
-    call print_1darray(tval, shape(2))
-    write(*,*) "Test 6 expected values"
-    call print_1darray(expected, shape(2))
+    write(*,*) "Test 1"
+    call print_1darray(tval, shape)
+    call print_1darray(expected, shape)
 
-    code = BMI_SUCCESS
-    do i = 1, shape(2)
-       if (isreal4equalreal4(expected(i), tval(i)).neqv..TRUE.) then
-          code = BMI_FAILURE
-          exit
+    status = BMI_SUCCESS
+    do i = 1, size
+       if (tval(i).ne.expected(i)) then
+          status = BMI_FAILURE
        end if
     end do
-  end function test6
+  end function test1
   
-  !function run_test() result(code)
-  !  type (bmi_heat) :: m
-  !  real, allocatable :: tval(:)
-  !  integer :: i
-  !  integer :: code
-  !
-  !  allocate(tval(size(indices)))
-  !
-  !  status = m%initialize(config_file)
-  !  status = m%get_value_at_indices(var_name, tval, indices)
-  !  status = m%finalize()
-  !
-  !  ! Visual inspection.
-  !  do i = 1, shape(2)
-  !     write(*,*) indices(i), tval(i), expected(i)
-  !  end do
-  !
-  !  code = BMI_SUCCESS
-  !  do i = 1, shape(2)
-  !     if (tval(i).ne.expected(i)) then
-  !        code = BMI_FAILURE
-  !        exit
-  !     end if
-  !  end do
-  !
-  !  deallocate(tval)
-  !end function run_test
+! Test getting r64 by nhru
+    function test2() result(code)
+    character (len=*), parameter :: &
+         var_name = "pfr_dunnian_flow"
+    integer, parameter :: rank =1
+    integer, parameter :: size = 7
+    integer, parameter, dimension(rank) :: shape = (/ 7 /)
+    integer, parameter, dimension(size) :: &
+       indices = (/ 2, 4, 6, 8, 10, 12, 14 /)
+
+    double precision, parameter :: expected(size) = (/ 0.00D+000, 0.00D+000, 0.00D+000, 0.00D+000, &
+                      0.00D+000, 0.00D+000, 0.00D+000 /)
+    double precision :: val(size)
+    integer :: i, code
+    double precision :: endtime
+
+    code = m%initialize(config_file)
+    code = m%get_end_time(endtime)
+    do i = 1,int(endtime)
+        code = m%update()
+        if(i == endtime) then
+            code = m%get_value_at_indices(var_name, val, indices)
+        endif
+    enddo
+    
+    ! Visual inspection.
+    write(*,*) "Test 2"
+    write(*,*) "Expected"
+    call print_d_1darray(expected, shape)
+    write(*,*) "get Value"
+    call print_d_1darray(val, shape)
+
+    code = BMI_SUCCESS
+    do i = 1, size
+       if (val(i).ne.expected(i)) then
+          code = BMI_FAILURE
+       end if
+    end do
+  end function test2
 
 end program test_get_value_at_indices
